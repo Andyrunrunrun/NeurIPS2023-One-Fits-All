74b2d3436178e39b4fcc9c445e22f2f1858691d3
Andyrunrunrun <2632880737@qq.com>
2025-07-23 19:02:33 +0800

    s

--- Long-term_Forecasting/scripts/ETTh1.sh
+++ Long-term_Forecasting/scripts/ETTh1.sh
@@ -1,5 +1,5 @@
 
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=5
 
 seq_len=336
 model=MultiModelTS
@@ -12,7 +12,7 @@ for lr in 0.0001
 do
 
 python main.py \
-    --root_path /dev_data/hh/multimodel/NeurIPS2023-One-Fits-All/datasets/ETT-small \
+    --root_path /chenyaofo/andy/new_project/NeurIPS2023-One-Fits-All/datasets/ETT-small \
     --data_path ETTh1.csv \
     --model_id ETTh1_$model'_'$model_layer'_'$seq_len'_'$pred_len'_'$percent \
     --base_model gpt2 \
@@ -35,13 +35,13 @@ python main.py \
     --patch_size 16 \
     --stride 8 \
     --percent $percent \
-    --model_layer 6 \
+    --model_layer 100 \
     --itr 1 \
     --model $model \
     --tmax 20 \
     --cos 1 \
     --is_gpt 1 \
-    --base_model /dev_data/hh/model/Qwen2.5-0.5B
+    --base_model /chenyaofo/hf_models/Llama-2-7b-hf
 
 done
 done
